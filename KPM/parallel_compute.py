74cb2862dc8b2ac63bb03ed738678166112a0786
Chris <cal422@lehigh.edu>
2026-03-10 21:12:43 -0400

    Updated the parallel_compute module in the KPM package such that its dependencies now adhere to the new minimal package hierarchy as well as updating the vacancies ADOS parallel compute function so it is compatible with the newest vacancy generating codes for hyperbolic q=3 Hamiltonians.

--- KPM/parallel_compute.py
+++ KPM/parallel_compute.py
@@ -3,7 +3,6 @@ from KPM.KPM import moments_ADOS_general
 import numpy as np
 from KPM.measure import rescale_disorder_unity_window, rescale_operator_unity_window
 import scipy.sparse
-from Fundamental.vacancies import general_q3_hamiltonian_equal_sublattice_vacancy_density_add_on_top
 import pickle
 
 
@@ -43,36 +42,38 @@ def run_KPM_ADOS_parallel_disorder(n_jobs, sparse_ham, W_list, number_moments,
         )
 
 
-def run_KPM_ADOS_parallel_vacancies_q3(n_jobs, sparse_tbham, eigval_min, eigval_max,
+def run_KPM_ADOS_parallel_vacancies_q3(n_jobs, eigval_min, eigval_max,
                                        pval, nval, vac_density_list, number_moments,
                                        num_rand_vecs, rand_vec_dim,
-                                       save_dir, save_name, save_index_list):
+                                       save_dir, save_name, save_index_list,
+                                       random_seeds, vacancy_hyperbolicq3_func):
 
-    def run_routine(sparse_ham, number_moments, random_vecs_arr, save_dir, save_name, save_index):
-        result = moments_ADOS_general(sparse_ham, number_moments, random_vecs_arr)
-        np.save(save_dir + '/' + save_name + '_' + save_index, result)
+    def run_routine(sparse_ham, nummoments, random_vecs_arr, savedir, savename, save_index):
+        result = moments_ADOS_general(sparse_ham, nummoments, random_vecs_arr)
+        np.save(savedir + '/' + savename + '_' + save_index, result)
 
     with parallel_config(backend='loky'):
         Parallel(n_jobs=n_jobs)(
             delayed(run_routine)
-            (rescale_operator_unity_window(general_q3_hamiltonian_equal_sublattice_vacancy_density_add_on_top(sparse_tbham,
-                                                                                                              pval,
-                                                                                                              nval,
-                                                                                                              vac_density),
-                                           eigval_min=eigval_min, eigval_max=eigval_max
-                                           ),
-            number_moments,
-            random_vectors_arr_generate(num_rand_vecs, rand_vec_dim),
-            save_dir,
-            save_name,
-            save_index)
-            for (vac_density, save_index) in zip(vac_density_list, save_index_list)
+            (
+                rescale_operator_unity_window(
+                    vacancy_hyperbolicq3_func(pval, nval, vac_density, seed=randseed, isocheck=True),
+                    eigval_min=eigval_min,
+                    eigval_max=eigval_max
+                ),
+                number_moments,
+                random_vectors_arr_generate(num_rand_vecs, rand_vec_dim),
+                save_dir,
+                save_name,
+                save_index
+            )
+            for (vac_density, randseed, save_index) in zip(vac_density_list, random_seeds, save_index_list)
         )
 
     metadata_dict = {
         'pval': pval,
         'nval': nval,
-        'Vacancy density list': vac_density_list,
+        '(Vacancy density, random seed, save_index)': zip(vac_density_list, random_seeds, save_index_list),
         'Number random vectors': num_rand_vecs,
         'Number moments up to': number_moments,
         'Eigenvalue extrema': (eigval_min, eigval_max)
